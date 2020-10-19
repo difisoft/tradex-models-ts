7110f5b9345c38bf51f632ef1b42153aa754d8b2
Developer <developer@techx.vn>
2020-10-19 11:06:08 +0700

    schema genarator by buildNumber:

--- index.ts
+++ index.ts
@@ -1,38 +1,6 @@
 
-import * as TradexModelsUserUtility from './services/user-utility';
-            
-import * as TradexModelsUser from './services/user';
-            
-import * as TradexModelsStrategy from './services/strategy';
-            
-import * as TradexModelsMarket from './services/market';
-            
-import * as TradexModelsConfiguration from './services/configuration';
-            
-import * as TradexModelsCommon from './services/common';
-            
-import * as TradexModelsBusinessInfo from './services/business-info';
-            
-import * as TradexModelsOrder from './services/order';
-            
 
 export {
 
-TradexModelsUserUtility,
-            
-TradexModelsUser,
-            
-TradexModelsStrategy,
-            
-TradexModelsMarket,
-            
-TradexModelsConfiguration,
-            
-TradexModelsCommon,
-            
-TradexModelsBusinessInfo,
-            
-TradexModelsOrder,
-            
 }
 
