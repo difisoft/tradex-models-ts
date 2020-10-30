404ff588f4d9783020cd0074267acc70acbdefe4
Developer <developer@techx.vn>
2020-10-30 02:06:42 +0000

    schema genarator by buildNumber: 352

--- index.ts
+++ index.ts
@@ -1,50 +1,38 @@
 
-import * as TradexModelsCommon from './services/common';
-            
-import * as TradexModelsAiAdvisor from './services/ai-advisor';
-            
-import * as TradexModelsBasketOrder from './services/basket-order';
-            
-import * as TradexModelsBusinessInfo from './services/business-info';
-            
-import * as TradexModelsConfiguration from './services/configuration';
-            
-import * as TradexModelsMarket from './services/market';
-            
-import * as TradexModelsOrder from './services/order';
-            
-import * as TradexModelsStrategy from './services/strategy';
+import * as TradexModelsUserUtility from './services/user-utility';
             
 import * as TradexModelsUser from './services/user';
             
-import * as TradexModelsUserUtility from './services/user-utility';
+import * as TradexModelsStrategy from './services/strategy';
             
-import * as TradexModelsVcscRest from './services/vcsc-rest';
+import * as TradexModelsMarket from './services/market';
+            
+import * as TradexModelsConfiguration from './services/configuration';
+            
+import * as TradexModelsCommon from './services/common';
+            
+import * as TradexModelsBusinessInfo from './services/business-info';
+            
+import * as TradexModelsOrder from './services/order';
             
 
 export {
 
-TradexModelsCommon,
-            
-TradexModelsAiAdvisor,
-            
-TradexModelsBasketOrder,
-            
-TradexModelsBusinessInfo,
-            
-TradexModelsConfiguration,
-            
-TradexModelsMarket,
-            
-TradexModelsOrder,
-            
-TradexModelsStrategy,
+TradexModelsUserUtility,
             
 TradexModelsUser,
             
-TradexModelsUserUtility,
+TradexModelsStrategy,
             
-TradexModelsVcscRest,
+TradexModelsMarket,
+            
+TradexModelsConfiguration,
+            
+TradexModelsCommon,
+            
+TradexModelsBusinessInfo,
+            
+TradexModelsOrder,
             
 }
 
